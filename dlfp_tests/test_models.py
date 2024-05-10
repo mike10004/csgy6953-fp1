504bbf200c9c87715e8809ea727b788044ca68cc
Mike Chaberski <mchaberski@gmail.com>
2024-05-10 00:29:01 -0400

    test_models: show input sizes of layers

--- dlfp_tests/test_models.py
+++ dlfp_tests/test_models.py
@@ -2,12 +2,18 @@ import itertools
 from unittest import TestCase
 
 import tabulate
+import torch.random
+from torch.utils.data import DataLoader
 
+import dlfp.models
 from dlfp.main import CruciformerRunner
 from dlfp.models import ModelHyperparametry
 from dlfp.models import create_model
 import torchsummary
+import dlfp_tests.tools
+from dlfp.train import Trainer
 
+dlfp_tests.tools.suppress_cuda_warning()
 
 class ModuleMethodsTest(TestCase):
 
@@ -33,3 +39,32 @@ class ModuleMethodsTest(TestCase):
             hyperparameter_counts.append(summary.trainable_params)
         print(tabulate.tabulate(table, headers=headers, tablefmt="simple_grid"))
         self.assertEqual(len(hyperparameter_counts), len(set(hyperparameter_counts)), "expect unique parameter counts")
+
+    def test_shapes(self):
+        cr = CruciformerRunner()
+        dataset = cr.resolve_dataset()
+        bl = cr.create_bilinguist(dataset)
+        with torch.random.fork_rng():
+            torch.random.manual_seed(13251)
+            batch_size=31
+            train_loader = DataLoader(dataset.train, batch_size=batch_size, collate_fn=bl.collate, shuffle=True)
+            src_vocab_size = len(bl.source.vocab)
+            tgt_vocab_size = len(bl.target.vocab)
+            h = ModelHyperparametry()
+            model = dlfp.models.create_model(src_vocab_size=src_vocab_size, tgt_vocab_size=tgt_vocab_size, h=h)
+            device = dlfp_tests.tools.get_device()
+            model = model.to(device)
+            with torch.random.fork_rng():
+                torch.random.manual_seed(12345)
+                src, tgt = next(iter(train_loader))
+                src = src.to(device)
+                tgt = tgt.to(device)
+                print(src.shape, tgt.shape)
+                tgt_input = tgt[:-1, :]
+                src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = Trainer.create_mask_static(src, tgt_input, device=device, pad_idx=1)
+                # def forward(self, src: Tensor, trg: Tensor, src_mask: Tensor, tgt_mask: Tensor,
+                #                 src_padding_mask: Tensor, tgt_padding_mask: Tensor,
+                #                 memory_key_padding_mask: Tensor):
+                args = [src, tgt_input, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask]
+                # model(*args)
+                torchsummary.summary(model, input_data=args)
